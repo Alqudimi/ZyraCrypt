8c56537b220231e2eb1843c310b1f08107579a30
Alqudimi <abodx1234freey@gmail.com>
2025-10-02 20:51:03 +0300

    encryption system

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
-# Encryption System Core Package
+# Alqudimi Encryption System - Core Source Package
+
+"""
+Enterprise-grade cryptographic library providing advanced encryption services.
+
+This package provides access to all cryptographic modules through compiled
+binary extensions for maximum performance and source code protection.
+"""
+
+# Version information
+__version__ = "2.0.0"
+__author__ = "Alqudimi Systems"
+
+# Import core modules - these will be compiled to binary extensions
+# Note: These imports will work with both source and compiled versions
